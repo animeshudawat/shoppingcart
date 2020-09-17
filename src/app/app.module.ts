d59de7ce09f955d67ed3351b1bb3eee8bebca06b
udawaa <animesh.udawat@emc.com>
2020-09-17 21:22:34 +0530

    Adding base component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCatalogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-catalog/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    name: string;
+    price: number;
+    rating: number;
+    stockUnits: number;
+    image: string;
+}
